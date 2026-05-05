f548897c234a6643a222e9ed299c416c041d9a71
Alejandro Cruz Valera <alejandro.cruzv@elektra.com.mx>
2026-05-05 17:50:47 -0600

    refactor: reorganize project structure by moving components into specialized property and sections directories

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,17 @@
-import Navbar from "../components/Navbar";
-import HeroSearch from "../components/HeroSearch";
-import FeaturedCollections from "../components/FeaturedCollections";
-import NewInMarket from "../components/NewInMarket";
+import FeaturedCollections from '../components/FeaturedCollections';
+import HeroSearch from '../components/HeroSearch';
+import Navbar from '../components/Navbar';
+import NewInMarket from '../components/NewInMarket';
 
 export default function Home() {
-  return (
-    <>
-      <Navbar />
-      <main className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 pb-20">
-        <HeroSearch />
-        <FeaturedCollections />
-        <NewInMarket />
-      </main>
-    </>
-  );
+    return (
+        <>
+            <Navbar />
+            <main className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 pb-20">
+                <HeroSearch />
+                <FeaturedCollections />
+                <NewInMarket />
+            </main>
+        </>
+    );
 }
